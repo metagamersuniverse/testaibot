9b0455a2af66a94395d665e0cf8bd8e0ca9ec045
SSSS <113474082+metagamersuniverse@users.noreply.github.com>
2023-02-14 00:18:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def _(event):
         'task': "image_captioning",
     }
     output = version.predict(**inputs)
-    await event.reply(output)
+    await event.reply(output, caption=None)
 
 imagebot.start(bot_token=BOT_TOKEN)
 imagebot.run_until_disconnected()
